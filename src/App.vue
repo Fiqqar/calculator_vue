0a0a4682958fbdcadaca5b8e6da1710ed4b50b8e
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-03-29 20:30:18 +0700

    29-03-24 (making function for the button)
    done
    -making function for number and operator

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<CalculatorComp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CalculatorComp from "./components/CalculatorComp.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalculatorComp
   }
 }
 </script>
@@ -23,4 +21,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style>./components/CalculatorComp.vue
